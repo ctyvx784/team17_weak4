1023de8237555b52fa49792e2ab3b047a16bd789
Whoseop-Song <whoseop.song@gmail.com>
2016-03-31 13:20:40 +0900

    getinfo.java update (prototype)

--- bill/src/main/java/bill/getinfo.java
+++ bill/src/main/java/bill/getinfo.java
@@ -4,25 +4,37 @@ import java.util.*;
 
 public class getinfo {
 	
-	
+	String plan;
+	int line, usage;
+	Scanner scan;
+	public getinfo(){
+		scan=new Scanner(System.in);
+		System.out.print("Input your plan : ");
+		setplan(scan.next());
+		System.out.print("Input your number of line : ");
+		setline(scan.nextInt());
+		System.out.print("Input your usage : ");
+		setusage(scan.nextInt());
+	}
+
 	private void setplan(String plan){
-		
+		this.plan=plan;
 	}
 	public String getplan(){
-		return null;
+		return plan;
 	}
 	
 	
 	private void setline(int line){
-		
+		this.line=line
 	}
 	public int getline(){
-		return 0;
+		return line;
 	}
 	
 	
 	private void setusage(int usage){
-		
+		this.usage=usage
 	}
 	public int getusage(){
 		return 0;
