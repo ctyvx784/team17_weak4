fdf03d79fadc8e0945b6959213048e43b41fe791
FakerLulu <kjmjskr@gmail.com>
2016-03-31 22:54:45 +0900

    App.java privateConstructor

--- bill/src/main/java/bill/App.java
+++ bill/src/main/java/bill/App.java
@@ -2,8 +2,12 @@ package bill;
 
 public class App 
 {
+	private App(){
+		
+	}
     public static void main( String[] args )
     {
+    	
     	getinfo info=new getinfo();
     	make_bill bill= new make_bill(info.getplan(),info.getline(),info.getusage());
     	System.out.println("your total bill is "+bill.total_bill());
