8dabf006d5cc535be4e1ca82ec23f2d48e24b8af
Whoseop-Song <whoseop.song@gmail.com>
2016-04-01 00:21:23 +0900

    중괄호 수정

--- bill/src/main/java/bill/App.java
+++ bill/src/main/java/bill/App.java
@@ -1,13 +1,11 @@
 package bill;
 
-public class App 
-{
+public class App{
+	
 	private App(){
 		
 	}
-    	public static void main( String[] args )
-   	{
-    	
+    	public static void main( String[] args ){   	{
    	 	Getinfo info=new Getinfo();
    	 	Makebill bill= new Makebill(info.getPlan(),info.getLine(),info.getUsage());
     		System.out.println("your total bill is "+bill.totalBill());
