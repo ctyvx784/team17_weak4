b213a2e4c3aa2734fb4850e1e8a0ee8f43c3d909
Whoseop-Song <whoseop.song@gmail.com>
2016-03-31 20:47:02 +0900

    틀린부분 수정

--- bill/src/main/java/bill/App.java
+++ bill/src/main/java/bill/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main( String[] args )
     {
     	getinfo info=new getinfo();
-    	make_bill bill= new make_bill(getinfo.getplan(),getinfo.getline(),getinfo.getusage());
+    	make_bill bill= new make_bill(info.getplan(),info.getline(),info.getusage());
     	Syetem.out.println("your total bill is "+bill.total_bill());
     }
 }
