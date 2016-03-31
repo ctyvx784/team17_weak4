b038e38bbad449a093fdc6b728f23e3c2bd44bef
Whoseop-Song <whoseop.song@gmail.com>
2016-04-01 00:57:20 +0900

    상수 부분에 static final 선언

--- bill/src/main/java/bill/PlanSilver.java
+++ bill/src/main/java/bill/PlanSilver.java
@@ -1,11 +1,11 @@
 package bill;
 
 public class PlanSilver implements Plan{
-	double basiccost=29.95;
-	double overchargepermin=21.50;
-	double additionallinecost0.54;
-	double familydiscount=5;
-	public  PlanSilver(){
+	static final double basiccost=29.95;
+	static final double overchargepermin=21.50;
+	static final double additionallinecost0.54;
+	static final double familydiscount=5;
+	static final public  PlanSilver(){
 		
 	}
 	public double getBasicCost() {
