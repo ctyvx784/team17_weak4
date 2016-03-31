52e2730d2f8a5f45e51c945ed21194de13ef2504
Whoseop-Song <whoseop.song@gmail.com>
2016-04-01 00:50:07 +0900

    getinfo 부분  삭제 후 main에서 입력받음

--- bill/src/main/java/bill/App.java
+++ bill/src/main/java/bill/App.java
@@ -1,13 +1,21 @@
 package bill;
 
+import java.util.*;
+
 public class App{
 	
 	private App(){
 		
 	}
     	public static void main( String[] args ){   	{
-   	 	Getinfo info=new Getinfo();
-   	 	Makebill bill= new Makebill(info.getPlan(),info.getLine(),info.getUsage());
+   	 	scan=new Scanner(System.in);
+		System.out.print("Input your plan : ");
+		private String plan = scan.next();
+		System.out.print("Input your number of line : ");
+		private int line = scan.nextInt();
+		System.out.print("Input your usage : ");
+		private int usage scan.nextInt();
+   	 	Makebill bill= new Makebill(plan,line,usage);
     		System.out.println("your total bill is "+bill.totalBill());
    	}
 }
