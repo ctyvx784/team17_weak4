ec774113e067254e753ddf660a229d153a824def
Whoseop-Song <whoseop.song@gmail.com>
2016-03-31 20:34:23 +0900

    main prototype

--- bill/src/main/java/bill/App.java
+++ bill/src/main/java/bill/App.java
@@ -4,6 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
+    	getinfo info=new getinfo();
+    	make_bill bill= new make_bill(getinfo.getplan(),getinfo.getline(),getinfo.getusage());
+    	Syetem.out.println("your total bill is "+bill.total_bill());
     }
 }
