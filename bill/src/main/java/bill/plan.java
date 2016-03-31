6f11f49e9fbe0a223d3ab1c2e45833c307e94750
Whoseop-Song <whoseop.song@gmail.com>
2016-03-31 21:49:43 +0900

    add interface

---
+++ bill/src/main/java/bill/plan.java
@@ -0,0 +1,8 @@
+package bill;
+
+public interface plan {
+	public double get_basic_cost();
+	public double get_overcharge_cost();
+	public double get_additional_line_cost();
+	public double get_family_discountcost();
+}
