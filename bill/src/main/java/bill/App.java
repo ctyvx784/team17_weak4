407652a56fbfe81387ab0df00faccbca9d85ad41
Whoseop-Song <whoseop.song@gmail.com>
2016-04-02 22:03:24 +0900

    변수 선언과 입력을 구분

--- bill/src/main/java/bill/App.java
+++ bill/src/main/java/bill/App.java
@@ -8,13 +8,16 @@ public class App{
 		
 	}
     	public static void main( String[] args ){   	
+   	 	private String plan;
+   	 	private int line;
+   	 	private int usage;
    	 	scan=new Scanner(System.in);
 		System.out.print("Input your plan : ");
-		private String plan = scan.next();
+		plan = scan.next();
 		System.out.print("Input your number of line : ");
-		private int line = scan.nextInt();
+		line = scan.nextInt();
 		System.out.print("Input your usage : ");
-		private int usage scan.nextInt();
+		usage = scan.nextInt();
    	 	Makebill bill= new Makebill(plan,line,usage);
     		System.out.println("your total bill is "+bill.totalBill());
    	}
